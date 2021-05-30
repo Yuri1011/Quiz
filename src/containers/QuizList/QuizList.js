856932250f84ba49299ab29b2af49ccc2259dc45
Yuri <dog-pit10@yandex.ru>
2021-05-30 16:04:44 +0300

    added functionality in menu list quiz.

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,9 +1,31 @@
 import React from "react";
+import c from "./QuizList.module.css";
+import {NavLink} from "react-router-dom";
 
 class QuizList extends React.Component {
+
+    renderQuizes = () => {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li key={index}>
+                    <NavLink to={'/quiz/' + quiz}>
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>QuizList</div>
+            <div className={c.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+                    <ul>
+                        {this.renderQuizes()}
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
