682780054a7c6d62cf56fc01d19c2a468242295f
Yuri <dog-pit10@yandex.ru>
2021-05-30 16:35:14 +0300

    authorization form.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,33 @@
 import React from "react";
+import c from "./Auth.module.css";
+import Button from "../../components/UI/Button/Button";
 
 class Auth extends React.Component {
+
+    loginHandler = () => {
+
+    }
+    registerHandler = () => {
+
+    }
+    submitForm = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <h1>
-                    Auth
-                </h1>
+            <div className={c.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitForm} className={c.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <Button type={'success'} onClick={this.loginHandler}>Войти</Button>
+                        <Button type={'primary'} onClick={this.registerHandler}>Авторизация</Button>
+                    </form>
+                </div>
             </div>
         )
     }
