a61985f0858579b3ff00d0578373e7ea13692176
Yuri <dog-pit10@yandex.ru>
2021-05-31 16:28:50 +0300

    validation input .

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,35 @@ import Input from "../../components/UI/Input/Input";
 
 class Auth extends React.Component {
 
+    state = {
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Введите корректный email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Введите корректный password',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+    }
+
     loginHandler = () => {
 
     }
@@ -15,6 +44,27 @@ class Auth extends React.Component {
         e.preventDefault();
     }
 
+    onChangeHandler=(event,controlName)=>{
+
+    }
+
+    renderInputs = () => {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input key={controlName + index}
+                       value={control.value}
+                       valid={control.valid}
+                       touched={control.touched}
+                       label={control.label}
+                       errorMessage={control.errorMessage}
+                       shouldValidate={!!control.validation}
+                       onChange={(event) => this.onChangeHandler(event, controlName)}
+                       type={control.type}/>
+            )
+        })
+    }
+
     render() {
         return (
             <div className={c.Auth}>
@@ -22,8 +72,10 @@ class Auth extends React.Component {
                     <h1>Авторизация</h1>
 
                     <form onSubmit={this.submitForm} className={c.AuthForm}>
-                        <Input label={'Email'}/>
-                        <Input label={'Password'}/>
+
+                        {
+                            this.renderInputs()
+                        }
 
                         <Button type={'success'} onClick={this.loginHandler}>Войти</Button>
                         <Button type={'primary'} onClick={this.registerHandler}>Авторизация</Button>
